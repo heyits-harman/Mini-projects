8aa816a8c0c029ca722314dd748556edbac0262d
heyits-harman <harrygill7664@gmail.com>
2025-07-26 13:39:00 +0530

    Add files via upload

---
+++ Day 3/sentences.js
@@ -0,0 +1,7 @@
+export const sentences = [
+  "The quick brown fox jumps over the lazy dog",
+  "JavaScript is fun and powerful",
+  "Typing fast requires focus and rhythm",
+  "Code every day to sharpen your skills",
+  "Practice makes perfect"
+];
